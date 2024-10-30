1287dafdbe34b1636779d6a6aad228a0ff2c734f
Marcelo Andrioni <marceloandrioni@gmail.com>
2024-10-30 08:50:36 -0300

    direction in degrees in the 0 to 360 range

--- src/main/java/org/example/VectorDirection.java
+++ src/main/java/org/example/VectorDirection.java
@@ -31,7 +31,7 @@ public class VectorDirection extends Vectorize {
         try {
             double u_val = uVar.read(indexToCoords((int)num), this.n_dimensional_array).getDouble(0);
             double v_val = vVar.read(indexToCoords((int)num), this.n_dimensional_array).getDouble(0);
-            return Math.atan2(v_val, u_val);
+            return ((Math.toDegrees(Math.atan2(v_val, u_val)) % 360.0) + 360.0) % 360.0;
         } catch (Exception ex) {
             logger.error(ex);
             return Double.NaN;
